f4dd4292b8befa3cadd1c0a60e9a4e34964fa5d7
benps <benpsnook@gmail.com>
2025-02-04 16:12:14 -0600

    Initial Commit

---
+++ src/EventTester.java
@@ -0,0 +1,10 @@
+import java.time.LocalDateTime;
+
+public class EventTester {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    LocalDateTime now = LocalDateTime.now();
+}
